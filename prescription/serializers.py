29edd82d94e31a4afa90ee8ad68a9f274c8cf233
SinaBolouki <sina.bolouki@gmail.com>
2021-12-24 18:10:02 +0330

    added admin prescriptions list

--- prescription/serializers.py
+++ prescription/serializers.py
@@ -17,3 +17,12 @@ class PatientPrescriptionSerializer(ModelSerializer):
     class Meta:
         model = Prescription
         fields = ['prescriber', 'patient', 'drug_list', 'creation_date', 'prescriber_name']
+
+
+class AdminPrescriptionSerializer(ModelSerializer):
+    prescriber_name = serializers.CharField(source='prescriber.name')
+    patient_name = serializers.CharField(source='patient.name')
+
+    class Meta:
+        model = Prescription
+        fields = ['prescriber', 'patient', 'drug_list', 'creation_date', 'prescriber_name', 'patient_name']
--- prescription/views.py
+++ prescription/views.py
@@ -8,7 +8,7 @@ from rest_framework import viewsets
 
 from user.models import User
 from .models import Prescription
-from .serializers import PrescriptionSerializer, PatientPrescriptionSerializer
+from .serializers import PrescriptionSerializer, PatientPrescriptionSerializer, AdminPrescriptionSerializer
 
 
 class GetByDoctor(viewsets.ViewSet):
@@ -82,11 +82,31 @@ class GetByPatient(viewsets.ViewSet):
         if user.token_exp_time < django.utils.timezone.now():
             return HttpResponse('Token expired', status=409)
 
-        if not user.isDoctor:
-            return HttpResponse('User not doctor', status=409)
-
         prescriptions = Prescription.objects.filter(patient=user)
         prescriptions_ser = PatientPrescriptionSerializer(prescriptions, many=True)
         return HttpResponse(prescriptions_ser.data, status=200)
 
 
+class GetByAdmin(viewsets.ViewSet):
+
+    def handle_request(self, request):
+        try:
+            token = request.data['token']
+        except KeyError:
+            return HttpResponse('Required fields are empty!!!', status=406)
+        try:
+            user = User.objects.get(token=token)
+        except:
+            return HttpResponse('Token not valid', status=409)
+        if not user:
+            return HttpResponse('Token not valid', status=409)
+
+        if user.token_exp_time < django.utils.timezone.now():
+            return HttpResponse('Token expired', status=409)
+
+        if not user.isAdmin:
+            return HttpResponse('User not admin', status=409)
+
+        prescriptions = Prescription.objects.all()
+        prescriptions_ser = AdminPrescriptionSerializer(prescriptions, many=True)
+        return HttpResponse(prescriptions_ser.data, status=200)
