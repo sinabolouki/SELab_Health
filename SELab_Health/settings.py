a604ac7e48b5b695c986ddcda1851a2571455a54
SinaBolouki <sina.bolouki@gmail.com>
2021-12-25 00:14:07 +0330

    added admin to apps

--- SELab_Health/settings.py
+++ SELab_Health/settings.py
@@ -35,6 +35,7 @@ INSTALLED_APPS = [
     'user.apps.UserConfig',
     'prescription.apps.PrescriptionConfig',
     'gateway.apps.GatewayConfig',
+    'admin.apps.AdminConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
